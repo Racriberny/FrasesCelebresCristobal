84149ab2e40cdb48e1eb95cb7395c93418273dd1
Cristobal Bernal Mayordomo <90463533+Racriberny@users.noreply.github.com>
2023-03-17 15:44:16 +0100

    funciona!!!!

--- src/main/java/com/cristobalbernal/frasescelebrescristobal/FrasesCelebresCristobalApplication.java
+++ src/main/java/com/cristobalbernal/frasescelebrescristobal/FrasesCelebresCristobalApplication.java
@@ -7,14 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FrasesCelebresCristobalApplication {
 
-    public static boolean DEBUG = true;
+    public static boolean DEBUG = false;
     public static String CONFIG_FILE =  null;
 
     public static void main(String[] args) {
         SpringApplication.run(FrasesCelebresCristobalApplication.class, args);
     }
 
-    public void run(String... args) throws Exception {
-        Log.i("TAG", "Texto cualquiera");
-    }
 }
